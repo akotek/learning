4b88e9fe3a4db9312420b1eeb943bfc30807febc
Aviv Kotek <kotekmath@gmail.com>
2020-01-22 18:02:38 +0200

    first commit

---
+++ clj/i_am_a_horse_in_the_land_of_booleans.clj
@@ -0,0 +1,42 @@
+(ns i-am-a-horse-in-the-land-of-booleans
+  (:refer-clojure :exclude [boolean]))
+
+(defn boolean [x]
+  (if (or (nil? x) (false? x)) false true))
+
+(defn abs [x]
+  (if (< x 0) (* x -1) x))
+
+(defn divides? [divisor n]
+  (== (mod n divisor) 0))
+
+(defn fizzbuzz [n]
+  (cond
+    (and (divides? 15 n)) "gotcha!"
+    (divides? 3 n) "fizz"
+    (divides? 5 n) "buzz"
+    :else           ""))
+
+
+(defn teen? [age]
+  (and (> age 12) (< age 20)))
+
+(defn not-teen? [age]
+  (not (teen? age)))
+
+(defn generic-doublificate [x]
+  (cond
+    (number? x) (* 2 x)
+    (empty? x) nil
+    (or (list? x) (vector? x)) (* 2 (count x))
+    :else         true))
+
+(defn leap-year? [year]
+  (cond
+    (divides? 400 year) true
+    (divides? 100 year) false
+    (divides? 4 year) true
+    :else             false
+    ))
+
+; '_______'
