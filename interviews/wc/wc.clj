d16b46e234b515d81a06d4fd05cc1b765389f92b
Aviv Kotek <kotekmath@gmail.com>
2021-08-10 22:05:01 +0300

    Add files via upload

---
+++ interviews/wc/wc.clj
@@ -0,0 +1,28 @@
+(ns exercises.wc
+  (:require [clojure.java.io :as io]
+            [clojure.string :as s]))
+
+(defn wc1 [fp]
+  ;; read whole file into string at once
+  (-> fp
+      slurp
+      s/lower-case
+      (s/split #"_+")
+      frequencies))
+
+
+(defn wc2 [fp]
+  ;; iterate line by line
+  (with-open [rdr (io/reader fp)]
+    (reduce
+      (fn [res line]
+        (merge-with + res (-> line
+                              s/lower-case
+                              (s/split #"_+")
+                              frequencies)))
+      {} (line-seq rdr))))
+
+
+(def fp "src/exercises/words")
+(sort-by val > (wc1 fp))
+(wc2 fp)
