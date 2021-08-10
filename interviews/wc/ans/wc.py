a7e9e9347abb716cfc2b4b20246b37860554457d
Aviv Kotek <kotekmath@gmail.com>
2021-08-10 23:56:55 +0300

    Add files via upload

--- interviews/wc/ans/wc.py
+++ interviews/wc/ans/wc.py
@@ -7,7 +7,7 @@ with open("words") as file:
         for w in splitted:
             m[w] = m.get(w, 0) + 1
 
-for k,v in m.items():
+for k,v in sorted(m.items(), key=lambda x:x[1], reverse=True):
     print("{}:{}".format(k, v))
 
 print("total!!!: ", sum(m.values()))
