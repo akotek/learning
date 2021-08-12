2334f80a10fe0fadea5631ac7b74690dc7f61ba3
Aviv Kotek <kotekmath@gmail.com>
2021-08-12 09:50:58 +0300

    Add files via upload

--- interviews/bh/ans/bhistory.py
+++ interviews/bh/ans/bhistory.py
@@ -1,3 +1,5 @@
+# ===================================================
+# PART 1:
 class BrowserHistory:
     def __init__(self, url: str):
         self.current = [url]
@@ -8,6 +10,7 @@ class BrowserHistory:
 
     def visit(self, url: str) -> None:
         self.current.append(url)
+        self.future = []  # reset the forward stack
 
     def back(self, steps: int) -> str:
         while steps > 0 and len(self.current) > 1:
@@ -20,3 +23,39 @@ class BrowserHistory:
             self.current.append(self.future.pop())
             steps -= 1
         return self.current[-1]
+
+
+# ===================================================
+# PART 2:
+class Tab:
+    # BrowserHistory is now Tab
+    pass
+
+
+class BrowserHistory2:
+    def __init__(self):
+        self.tabs = []
+
+    def run_tab(self, tab: int) -> Tab:
+        pass
+
+# ===================================================
+# PART 3:
+# OPT1, DB:
+# CREATE TABLE Table1(
+#    session_id  INT PRIMARY KEY NOT NULL,
+#    history     VARCHAR(100)  NOT NULL,    JSON: {"current": ["url1", "url2], "history": ["url3"]}
+# );
+
+# many rows representing the history, with order column
+# CREATE TABLE Table2(
+#    session_id  INT PRIMARY KEY NOT NULL,
+#    history     VARCHAR(100)  NOT NULL,    "url1" (1x instance)
+#    order       INT  NOT NULL;             # idx=0
+# );
+
+# OPT2, DISK:
+# One file, each line represents a session, separated by ',' for current/history
+#   ["url1", "url2"], ["url3"]
+# File-per-session/tab, each file represents session/tab
+# ...
